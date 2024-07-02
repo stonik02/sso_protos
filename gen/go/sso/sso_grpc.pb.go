5d15689e3a754da12bdb24a38589da8870c7614f
Denis <skitlsskitlovich@gmail.com>
2024-07-03 02:14:26 +0300

    Generate

--- gen/go/sso/sso_grpc.pb.go
+++ gen/go/sso/sso_grpc.pb.go
@@ -4,10 +4,11 @@
 // - protoc             v4.22.3
 // source: sso/sso.proto
 
-package ssov1
+package sso
 
 import (
 	context "context"
+
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
